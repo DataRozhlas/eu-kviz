ffdfe807052bd8c39382a0f94d7cc26d43e763b5
Petr Kočí <petr.koci@samizdat.cz>
2024-04-02 15:08:40 +0200

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import questions from "./assets/questions.json"
 
-import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
 
 import './App.css'
 
 function App() {
 
   return (
-    <RadioGroup defaultValue="option-one">
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="option-one" id="option-one" />
-        <Label htmlFor="option-one">Option One</Label>
-      </div>
-      <div className="flex items-center space-x-2">
-        <RadioGroupItem value="option-two" id="option-two" />
-        <Label htmlFor="option-two">Option Two</Label>
-      </div>
-    </RadioGroup>
+    <div className="w-full max-w-[620px] mx-auto">
+      {questions.map((question, index) => {
+        return (
+          <div key={crypto.randomUUID()}>
+            <h2 className="text-xl font-semibold mb-4">{`${index + 1}. ${question.question}`}</h2>
+            <ul>
+              {question.answers?.map((answer, index) => {
+                return (
+                  <li key={crypto.randomUUID()}>
+                    <input type="radio" name={question.question} value={answer.text} />
+                    <label>{answer.text}</label>
+                  </li>
+                )
+              })}
+            </ul>
+            <Button variant="outline" className="mt-4">Vyhodnotit</Button>
+            <Separator className="my-4" />
+          </div>
+        )
+      })}
+
+    </div>
   )
 }
 
